30c235447386fcaa08f8ae8b9efe0950ac0655c2
slim47 <slim@xetid.cu>
2023-07-19 10:52:25 -0400

    added tracking added searchpanel added icon

--- models/finances_entry.py
+++ models/finances_entry.py
@@ -6,12 +6,13 @@ class FinancesEntry(models.Model):
     _description = "Finances Entry"
 
 
-    date = fields.Date(string='Fecha')
-    description = fields.Char(string='Nota')
-    amount = fields.Integer(string='Cantidad')
+    date = fields.Date(string='Fecha', tracking=True)
+    # tracking=True means everytime the record is changed the system will output a message telling what changed
+    description = fields.Char(string='Nota', tracking=True)
+    amount = fields.Integer(string='Cantidad', tracking=True)
     currency = fields.Selection([('cup', 'CUP'), ('mlc', 'MLC'), ('usd', 'USD'), ('btc', 'BTC')], string="Moneda")
-    transaction = fields.Selection([('efectivo', 'Efectivo'), ('transferencia', 'Transferencia')], string="Tipo de Transaccion")
-    type = fields.Selection([('entrada', 'Entrada'), ('salida', 'Salida')], string="Tipo")
-    starred = fields.Boolean(string="Importante")
+    transaction = fields.Selection([('efectivo', 'Efectivo'), ('transferencia', 'Transferencia')], string="Tipo de Transaccion", tracking=True)
+    type = fields.Selection([('entrada', 'Entrada'), ('salida', 'Salida')], string="Tipo", tracking=True)
+    starred = fields.Boolean(string="Importante", tracking=True)
     # odoo understands active field as to archive or not
     active = fields.Boolean(string="Active", default=True)
